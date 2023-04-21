251ded3a4864806bea2d1df3665961ad0fa75fbe
Thomas Gorczynski <118868848+ThomasGorczynski21@users.noreply.github.com>
2023-04-20 23:07:32 -0600

    Debug

--- WebScrape.py
+++ WebScrape.py
@@ -1,3 +1,6 @@
+import sys
+print(sys.path)
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -10,9 +13,13 @@ def google_search(keyword):
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
         search_results = soup.find_all('div', class_='tF2Cxc')
-            for result in search_results:
+        for result in search_results:
                 title = result.find('h3', class_='LC20lb DKV0Md').text
                 link = result.find('a')['href']
                 print(f'Title: {title}\nURL: {link}\n')
     else:
         print(f'Request failed with status code {response.status_code}')
+
+if __name__ == '__main__':
+    keyword = input('Enter the keyword to search: ')
+    google_search(keyword)
