a11a1f0726448ba6595383780d256ab77ece3b02
Thomas Gorczynski <118868848+ThomasGorczynski21@users.noreply.github.com>
2023-04-20 23:17:40 -0600

    Notes

--- WebScrape.py
+++ WebScrape.py
@@ -2,6 +2,9 @@ import sys
 print(sys.path)
 sys.path.append('/Users/tom_8/AppData/Local/Packages/PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0/LocalCache/local-packages/Python311/site-packages')
 
+
+## this script is meant to search google, but unsure if they allow it.
+
 import requests
 from bs4 import BeautifulSoup
 
